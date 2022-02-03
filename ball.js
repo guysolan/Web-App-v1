66b497c8dc8809acf415c518b7c8322ab59e0174
Guy Solan <thetismedical@gmail.com>
2022-02-03 10:17:01 +0000

    ball out

--- ball.js
+++ ball.js
@@ -1,203 +1,203 @@
-const INITIAL_VELOCITY = 0.01
-const VELOCITY_INCREASE = 0.0
-const blue_spot = document.getElementById('blue-spot')
-const green_spot = document.getElementById('green-spot')
-const start_V = 0.01
+// const INITIAL_VELOCITY = 0.01
+// const VELOCITY_INCREASE = 0.0
+// const blue_spot = document.getElementById('blue-spot')
+// const green_spot = document.getElementById('green-spot')
+// const start_V = 0.01
 
-export default class Ball {
-  constructor(ballElem) {
-    this.ballElem = ballElem
+// export default class Ball {
+//   constructor(ballElem) {
+//     this.ballElem = ballElem
 
-    //Gender Randomly Assigned
-    if(Math.round(Math.random())===1){
-      this.male = true
-    } else {this.male = false}
+//     //Gender Randomly Assigned
+//     if(Math.round(Math.random())===1){
+//       this.male = true
+//     } else {this.male = false}
 
-    //Age Randomly Assigned between 18 and 100
-    this.age = randomNumberBetween(18, 100)
+//     //Age Randomly Assigned between 18 and 100
+//     this.age = randomNumberBetween(18, 100)
 
-    //Heart Rate, Body Temp and Respitory Rate normally distrubuted and adjusted by age 
-    this.heartRate = gaussian(79.1,14.5)+(this.age-50)/(randomNumberBetween(3,8))
-    this.bodyTemp = gaussian(36.6,0.149) - ((this.age-50)/100)*(-0.021)
-    this.respitoryRate = gaussian()
+//     //Heart Rate, Body Temp and Respitory Rate normally distrubuted and adjusted by age 
+//     this.heartRate = gaussian(79.1,14.5)+(this.age-50)/(randomNumberBetween(3,8))
+//     this.bodyTemp = gaussian(36.6,0.149) - ((this.age-50)/100)*(-0.021)
+//     this.respitoryRate = gaussian()
 
-    this.reset()
-  }
-
-
-
-  get x() {
-    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
-  }
-
-  set x(value) {
-    this.ballElem.style.setProperty("--x", value)
-  }
-
-  get y() {
-    return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
-  }
-
-  set y(value) {
-    this.ballElem.style.setProperty("--y", value)
-  }
-
-  rect() {
-    return this.ballElem.getBoundingClientRect()
-  }
-
-  reset() {
-    this.x = 75 + (Math.random() - 0.5) * 40
-    this.y = 50 + (Math.random() - 0.5) * 40
-    this.direction = {
-      x: 0
-    }
-
-    while (
-      Math.abs(this.direction.x) <= 0.99
-    ) {
-      const heading = randomNumberBetween(0, 2 * Math.PI)
-      this.direction = {
-        x: Math.cos(heading),
-        y: Math.sin(heading)
-      }
-    }
-    // this.velocity = INITIAL_VELOCITY
-    this.velocity = start_V
-  }
-
-  call(sensor_classname, spot, prob_on, prob_off, stop = false) {
-    if (this.ballElem.classList.contains(sensor_classname)) {
-      spot.classList.add('active')
-      if (Math.round(randomNumberBetween(0, prob_off)) === 5) {
-        if (stop) {
-          this.start()
-        }
-        this.ballElem.classList.remove(sensor_classname)
-      }
-    } else {
-      spot.classList.remove('active')
-      if (Math.round(randomNumberBetween(0, prob_on)) === 5) {
-        if (stop) {
-          this.stop()
-        }
-        this.ballElem.classList.add(sensor_classname)
-      }
-    }
-  }
+//     this.reset()
+//   }
 
 
-  stop() {
-    this.velocity = 0
-  }
 
-  start() {
-    this.velocity = start_V
-  }
+//   get x() {
+//     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+//   }
 
+//   set x(value) {
+//     this.ballElem.style.setProperty("--x", value)
+//   }
 
-  update(delta) {
+//   get y() {
+//     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"))
+//   }
 
-    let maxV = 0.5
-    let changeF = maxV / 10
+//   set y(value) {
+//     this.ballElem.style.setProperty("--y", value)
+//   }
 
-    let changeX = randomNumberBetween(-changeF, changeF)
-    let changeY = randomNumberBetween(-changeF, changeF)
+//   rect() {
+//     return this.ballElem.getBoundingClientRect()
+//   }
 
+//   reset() {
+//     this.x = 75 + (Math.random() - 0.5) * 40
+//     this.y = 50 + (Math.random() - 0.5) * 40
+//     this.direction = {
+//       x: 0
+//     }
 
-    if (this.direction.x > maxV) {
-      // console.log('Too quick X')
-      changeX = randomNumberBetween(-changeF, 0)
-    } else if (this.direction.x < -maxV) {
-      // console.log('Too quick X')
-      changeX = randomNumberBetween(0, changeF)
-    }
+//     while (
+//       Math.abs(this.direction.x) <= 0.99
+//     ) {
+//       const heading = randomNumberBetween(0, 2 * Math.PI)
+//       this.direction = {
+//         x: Math.cos(heading),
+//         y: Math.sin(heading)
+//       }
+//     }
+//     // this.velocity = INITIAL_VELOCITY
+//     this.velocity = start_V
+//   }
 
-    if (this.direction.y > maxV) {
-      // console.log('Too quick Y')
-      changeY = randomNumberBetween(0, changeF)
-
-    } else if (this.direction.x < -maxV) {
-      // console.log('Too quick Y')
-      changeY = randomNumberBetween(-changeF, 0)
-    }
-
-    let repulsePower = 2
-
-    if (this.x < 55) {
-      // console.log('too left')
-      let closeLeft = ((this.x - 50) / repulsePower)
-      changeX = randomNumberBetween(0, changeF) / closeLeft
-    } else if (this.x > 90) {
-      // console.log('too right')
-      let closeRight = ((95 - this.x) / repulsePower)
-      changeX = randomNumberBetween(-changeF, 0) / closeRight
-    }
-
-    if (this.y > 80) {
-      // console.log('too low')
-      let closeBottom = ((90 - this.y) / repulsePower)
-      changeY = randomNumberBetween(0, changeF) / closeBottom
-    } else if (this.y < 20) {
-      // console.log('too high')
-      let closeTop = ((this.y - 10) / repulsePower)
-      changeY = randomNumberBetween(-changeF, 0) / closeTop
-    }
-
-    this.call('video-call', blue_spot, 500, 80, true)
-    this.call('audio-call', green_spot, 300, 80, false)
-
-    document.addEventListener('keydown', function (event) {
-      // changeY = 0
-      // changeX = 0
+//   call(sensor_classname, spot, prob_on, prob_off, stop = false) {
+//     if (this.ballElem.classList.contains(sensor_classname)) {
+//       spot.classList.add('active')
+//       if (Math.round(randomNumberBetween(0, prob_off)) === 5) {
+//         if (stop) {
+//           this.start()
+//         }
+//         this.ballElem.classList.remove(sensor_classname)
+//       }
+//     } else {
+//       spot.classList.remove('active')
+//       if (Math.round(randomNumberBetween(0, prob_on)) === 5) {
+//         if (stop) {
+//           this.stop()
+//         }
+//         this.ballElem.classList.add(sensor_classname)
+//       }
+//     }
+//   }
 
 
-      if (event.key == 'ArrowUp') {
+//   stop() {
+//     this.velocity = 0
+//   }
 
-        changeY += 10
-      }
-    })
-
-
-    this.direction.x += changeX
-    this.direction.y -= changeY
-
-    this.x += (this.direction.x) * this.velocity * delta
-    this.y += (this.direction.y) * this.velocity * delta
-    // console.log(`this.y = ${this.y}`)
-
-    this.velocity += VELOCITY_INCREASE * delta
-
-  }
-}
-
-function randomNumberBetween(min, max) {
-  return Math.random() * (max - min) + min
-}
+//   start() {
+//     this.velocity = start_V
+//   }
 
 
-function gaussian(mean, stdev) {
-  let y2;
-  let use_last = false;
-  let y1;
-  if (use_last) {
-    y1 = y2;
-    use_last = false;
-  } else {
-    let x1, x2, w;
-    do {
-      x1 = 2.0 * Math.random() - 1.0;
-      x2 = 2.0 * Math.random() - 1.0;
-      w = x1 * x1 + x2 * x2;
-    } while (w >= 1.0);
-    w = Math.sqrt((-2.0 * Math.log(w)) / w);
-    y1 = x1 * w;
-    y2 = x2 * w;
-    use_last = true;
-  }
+//   update(delta) {
 
-  let retval = mean + stdev * y1;
-  if (retval > 0)
-    return retval;
-  return -retval;
-}
+//     let maxV = 0.5
+//     let changeF = maxV / 10
+
+//     let changeX = randomNumberBetween(-changeF, changeF)
+//     let changeY = randomNumberBetween(-changeF, changeF)
+
+
+//     if (this.direction.x > maxV) {
+//       // console.log('Too quick X')
+//       changeX = randomNumberBetween(-changeF, 0)
+//     } else if (this.direction.x < -maxV) {
+//       // console.log('Too quick X')
+//       changeX = randomNumberBetween(0, changeF)
+//     }
+
+//     if (this.direction.y > maxV) {
+//       // console.log('Too quick Y')
+//       changeY = randomNumberBetween(0, changeF)
+
+//     } else if (this.direction.x < -maxV) {
+//       // console.log('Too quick Y')
+//       changeY = randomNumberBetween(-changeF, 0)
+//     }
+
+//     let repulsePower = 2
+
+//     if (this.x < 55) {
+//       // console.log('too left')
+//       let closeLeft = ((this.x - 50) / repulsePower)
+//       changeX = randomNumberBetween(0, changeF) / closeLeft
+//     } else if (this.x > 90) {
+//       // console.log('too right')
+//       let closeRight = ((95 - this.x) / repulsePower)
+//       changeX = randomNumberBetween(-changeF, 0) / closeRight
+//     }
+
+//     if (this.y > 80) {
+//       // console.log('too low')
+//       let closeBottom = ((90 - this.y) / repulsePower)
+//       changeY = randomNumberBetween(0, changeF) / closeBottom
+//     } else if (this.y < 20) {
+//       // console.log('too high')
+//       let closeTop = ((this.y - 10) / repulsePower)
+//       changeY = randomNumberBetween(-changeF, 0) / closeTop
+//     }
+
+//     this.call('video-call', blue_spot, 500, 80, true)
+//     this.call('audio-call', green_spot, 300, 80, false)
+
+//     document.addEventListener('keydown', function (event) {
+//       // changeY = 0
+//       // changeX = 0
+
+
+//       if (event.key == 'ArrowUp') {
+
+//         changeY += 10
+//       }
+//     })
+
+
+//     this.direction.x += changeX
+//     this.direction.y -= changeY
+
+//     this.x += (this.direction.x) * this.velocity * delta
+//     this.y += (this.direction.y) * this.velocity * delta
+//     // console.log(`this.y = ${this.y}`)
+
+//     this.velocity += VELOCITY_INCREASE * delta
+
+//   }
+// }
+
+// function randomNumberBetween(min, max) {
+//   return Math.random() * (max - min) + min
+// }
+
+
+// function gaussian(mean, stdev) {
+//   let y2;
+//   let use_last = false;
+//   let y1;
+//   if (use_last) {
+//     y1 = y2;
+//     use_last = false;
+//   } else {
+//     let x1, x2, w;
+//     do {
+//       x1 = 2.0 * Math.random() - 1.0;
+//       x2 = 2.0 * Math.random() - 1.0;
+//       w = x1 * x1 + x2 * x2;
+//     } while (w >= 1.0);
+//     w = Math.sqrt((-2.0 * Math.log(w)) / w);
+//     y1 = x1 * w;
+//     y2 = x2 * w;
+//     use_last = true;
+//   }
+
+//   let retval = mean + stdev * y1;
+//   if (retval > 0)
+//     return retval;
+//   return -retval;
+// }
